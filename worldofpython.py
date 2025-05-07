c455a177ef1b9d6472b4ae6f6b60419dbf8b313c
excusez28 <maisatabiha@gmail.com>
2025-05-07 22:15:05 +0600

    worldofython

---
+++ worldofpython.py
@@ -0,0 +1,20 @@
+#creating different variables
+x = "Maisa"
+y = "28"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#Userinput
+name = input("Enter your favourite fruit: ")
+print("My favourite fruit is ", name)
+fruit = input()
+print(fruit,"sounds great")
+
+import keyword
+
+#print all python keywords
+print("Python keywords are..\n")
+print(keyword.kwlist)
